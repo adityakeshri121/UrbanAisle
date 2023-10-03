840ef34fa07460af0116772c31b314e354b20727
adityakeshri121 <adityakeshrisam1729@gmail.com>
2023-10-04 01:17:15 +0530

    category changes

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,37 +3,45 @@ import './Categories.css';
 import { Link } from "react-router-dom";
 const Categories = () => {
     return (
-        <div className="flex h-[80vh] gap-2 m-2">
+        <div className="categories">
+
             <div className="col flex-1">
-                <div className="row1 relative">
+                <div className="row relative">
                     <img src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                     <button className="absolute">
                         <Link to='/products/1'>Sale</Link>
                     </button>
                 </div>
 
-                <div className="row2">
+                <div className="row">
                     <img src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                 </div>
             </div>
+
             <div className="col flex-1">
-                <div className="row1">
+                <div className="row">
                     <img src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                 </div>
             </div>
+
             <div className="col flex-[2]">
-                <div className="row1 flex">
+                <div className="row">
                     <div className="col">
-                        <img src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <div className="row">
+                            <img src="https://images.pexels.com/photos/1192609/pexels-photo-1192609.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        </div>
                     </div>
                     <div className="col">
-                        <img src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        <div className="row">
+                            <img src="https://images.pexels.com/photos/2703202/pexels-photo-2703202.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className="row2">
+                <div className="row">
                     <img src="https://images.pexels.com/photos/1159670/pexels-photo-1159670.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                 </div>
             </div>
+
         </div>
     )
 }
